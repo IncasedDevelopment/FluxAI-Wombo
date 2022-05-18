623dcaf57e9fa850893188145917a0afe5cf147e
Incased Development <64618520+IncasedDevelopment@users.noreply.github.com>
2022-05-19 11:10:17 +1200

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,31 @@
+const madalee = valleri;
+function valleri(ltanya, myajah) {
+  const luevenia = horris();
+  return valleri = function (braelei, ilka) {
+    braelei = braelei - 111;
+    let elleny = luevenia[braelei];
+    return elleny;
+  }, valleri(ltanya, myajah);
+}
+(function (samala, malaky) {
+  const eko = valleri, kamayia = samala();
+  while (!![]) {
+    try {
+      const janifer = -parseInt(eko(111)) / 1 * (parseInt(eko(123)) / 2) + parseInt(eko(134)) / 3 * (-parseInt(eko(136)) / 4) + parseInt(eko(119)) / 5 * (-parseInt(eko(127)) / 6) + -parseInt(eko(124)) / 7 * (parseInt(eko(135)) / 8) + parseInt(eko(128)) / 9 + parseInt(eko(133)) / 10 + parseInt(eko(117)) / 11;
+      if (janifer === malaky) break; else kamayia.push(kamayia.shift());
+    } catch (lillyauna) {
+      kamayia.push(kamayia.shift());
+    }
+  }
+}(horris, 895488));
+let styles = new Map;
+styles[madalee(138)](1, "Synthwave"), styles[madalee(138)](2, madalee(126)), styles[madalee(138)](3, madalee(113)), styles[madalee(138)](4, "Steampunk"), styles[madalee(138)](5, madalee(130)), styles[madalee(138)](6, madalee(114)), styles[madalee(138)](7, "HD"), styles[madalee(138)](8, madalee(112)), styles.set(9, madalee(125)), styles[madalee(138)](10, "Dark Fantasy"), styles[madalee(138)](11, madalee(132)), styles[madalee(138)](12, "Festive"), styles[madalee(138)](13, "Baroque"), styles[madalee(138)](14, madalee(129)), styles[madalee(138)](15, madalee(137)), styles[madalee(138)](16, madalee(139)), styles.set(17, madalee(131)), styles[madalee(138)](18, madalee(121)), styles.set(19, madalee(120)), styles[madalee(138)](20, "Blacklight"), styles[madalee(138)](21, madalee(115)), styles[madalee(138)](22, madalee(140)), styles.set(23, madalee(122));
+let steps = new Map;
+function horris() {
+  const mamoru = ["63005998gLVaNt", "exports", "10RARBoB", "Moonwalker", "Rose Gold", "Surreal", "106742OxhDlI", "5406807QvfzYk", "Psychic", "Ukiyoe", "3052680fpckkH", "964836LULOwM", "Etching", "Fantasy Art", "Provenance", "Mystical", "759360pqYVws", "263370OCIpGd", "16qnHHsb", "68YIILaT", "S. Dali", "set", "Wuhtercuhler", "Ghibli", "18CyKehM", "Pastel", "No Style", "Vibrant", "Psychedelic", "steps"];
+  horris = function () {
+    return mamoru;
+  };
+  return horris();
+}
+steps[madalee(138)](1, 23), steps[madalee(138)](2, 21), steps[madalee(138)](3, 23), steps[madalee(138)](4, 23), steps[madalee(138)](5, 19), steps[madalee(138)](6, 20), steps[madalee(138)](7, 21), steps[madalee(138)](8, 21), steps[madalee(138)](9, 21), steps[madalee(138)](10, 20), steps[madalee(138)](11, 20), steps.set(12, 32), steps.set(13, 20), steps.set(14, 20), steps.set(15, 20), steps[madalee(138)](16, 20), steps[madalee(138)](17, 19), steps[madalee(138)](18, 19), steps[madalee(138)](19, 20), steps[madalee(138)](20, 20), steps[madalee(138)](21, 20), steps[madalee(138)](22, 20), steps[madalee(138)](23, 20), module.exports = styles, module[madalee(118)][madalee(116)] = steps;
