4e3e1788df898a81fc3e076b53348b9d784a1ceb
Incased Development <64618520+IncasedDevelopment@users.noreply.github.com>
2022-05-19 10:56:04 +1200

    Create rest.js

---
+++ rest.js
@@ -0,0 +1,163 @@
+const vedat = diavian;
+(function (aketzaly, adelene) {
+  const zackhary = diavian, syeda = aketzaly();
+  while (!![]) {
+    try {
+      const khennedi = parseInt(zackhary(322)) / 1 + -parseInt(zackhary(328)) / 2 + parseInt(zackhary(341)) / 3 * (parseInt(zackhary(330)) / 4) + parseInt(zackhary(336)) / 5 * (parseInt(zackhary(362)) / 6) + parseInt(zackhary(361)) / 7 + -parseInt(zackhary(349)) / 8 * (-parseInt(zackhary(327)) / 9) + -parseInt(zackhary(344)) / 10 * (parseInt(zackhary(343)) / 11);
+      if (khennedi === adelene) break; else syeda.push(syeda.shift());
+    } catch (kongpheng) {
+      syeda.push(syeda.shift());
+    }
+  }
+}(dahquan, 341480));
+function dahquan() {
+  const stefin = ["4595QAySGK", "write", "exports", "handle_cookies", "parse", "66sFqAQS", "request", "33BzfQFm", "4699950yDrAlm", "message", "cookies", "enter_queue", "headers", "5451952sVdJXl", "post", "end", "shift", "set-cookie", "bind", "push", "entries", "length", "_delay", "byteLength", "delay_handle", "3682434SfdQnk", "6pLIQVq", "object", '", path = "', "path", "cookies_enabled", "RestError", "statusCode", "delay", "error", "utf8", "split", "POST", "get", "https", "options", "toFriendly", "setEncoding", "OPTIONS", 'RestError(hostname = "', "custom_headers", "get_cookies", "494570fICzpu", "queue", "stringify", '"): "', "agent", "9gDrBfK", "984856LLarae", "captureStackTrace", "98336ASJqBU", "hostname", "string", "data", "GET", "scheduler"];
+  dahquan = function () {
+    return stefin;
+  };
+  return dahquan();
+}
+const https = require(vedat(314)), Rest = module[vedat(338)] = class Rest {
+  constructor(_0x17debd, _0x576429 = 250, _0x3f5d24 = !![]) {
+    const anakah = vedat;
+    this.hostname = _0x17debd, this[anakah(326)] = new https.Agent({keepAlive: !![], maxsockets: 1}), this.queue = [], this[anakah(358)] = _0x576429, this[anakah(360)] = null, this[anakah(346)] = {}, this.cookies_enabled = _0x3f5d24, this.custom_headers = {};
+  }
+  ["scheduler"]() {
+    const fritha = vedat;
+    if (this[fritha(323)].length) this.queue[fritha(352)]()(); else {
+      if (this[fritha(360)] !== null) clearInterval(this.delay_handle);
+      this[fritha(360)] = null;
+    }
+  }
+  [vedat(347)](_0x58a535) {
+    return new Promise((cyric, render) => {
+      const jager = diavian;
+      if (this._delay > 0) {
+        if (this[jager(360)] === null) this[jager(360)] = setInterval(this[jager(335)][jager(354)](this), this[jager(358)]);
+        this[jager(323)][jager(355)](() => {
+          _0x58a535(cyric, render);
+        });
+      } else _0x58a535(cyric, render);
+    });
+  }
+  [vedat(339)](_0x13d745) {
+    const tayma = vedat;
+    if (!_0x13d745) return;
+    if (!this[tayma(366)]) return;
+    for (let averitt of _0x13d745) {
+      let [_0xdef2e9, _0x520996] = averitt[tayma(372)]("="), [_0x3e595c] = _0x520996[tayma(372)](";");
+      this[tayma(346)][_0xdef2e9] = _0x3e595c;
+    }
+  }
+  [vedat(321)]() {
+    const danyael = vedat;
+    if (!this[danyael(366)]) return "";
+    return Object[danyael(356)](this.cookies).map(([_0x3305fb, _0x58252]) => _0x3305fb + "=" + _0x58252).join("; ");
+  }
+  ["post"](_0x54616d, _0x5b29ad, _0x54e8a5 = vedat(373), _0xff4b08) {
+    const dehlani = vedat;
+    return this[dehlani(347)]((orah, micalah) => {
+      const charlotteann = dehlani;
+      let clarence = JSON[charlotteann(324)](_0x5b29ad), mytrell = {agent: this[charlotteann(326)], hostname: this[charlotteann(331)], port: 443, path: _0x54616d, method: _0x54e8a5, headers: {Cookie: this[charlotteann(321)](), "Content-Type": "application/json; charset=utf-8", "Content-Length": Buffer[charlotteann(359)](clarence), ...this.custom_headers, ..._0xff4b08}};
+      const briam = https.request(mytrell, mahil => {
+        const harmie = charlotteann;
+        this.handle_cookies(mahil.headers[harmie(353)]);
+        let aideen = "";
+        mahil[harmie(317)]("utf8"), mahil.on(harmie(333), zaleigh => {
+          aideen += zaleigh;
+        }), mahil.on(harmie(351), () => {
+          const dillian = harmie;
+          if (mahil.statusCode !== 200) micalah(new RestError(this.hostname, _0x54616d, aideen)); else try {
+            orah(JSON[dillian(340)](aideen));
+          } catch (sendi) {
+            orah(aideen);
+          }
+        }), mahil.on(harmie(370), jedric => {
+          micalah(new RestError(this.hostname, _0x54616d, jedric));
+        });
+      });
+      briam.on("error", laderrian => {
+        const edney = charlotteann;
+        micalah(new RestError(this[edney(331)], _0x54616d, laderrian));
+      }), briam[charlotteann(337)](clarence), briam[charlotteann(351)]();
+    });
+  }
+  [vedat(374)](_0x13fadb, _0x337e36 = vedat(334), _0x43fb27 = {}) {
+    const arbedella = vedat;
+    return this[arbedella(347)]((celyna, broddrick) => {
+      const crishon = arbedella;
+      let arlandis = {agent: this[crishon(326)], hostname: this.hostname, port: 443, path: _0x13fadb, method: _0x337e36, headers: {Cookie: this[crishon(321)](), ...this[crishon(320)], ..._0x43fb27}};
+      const arnesto = https[crishon(342)](arlandis, tieler => {
+        const legand = crishon;
+        this[legand(339)](tieler[legand(348)][legand(353)]);
+        let inese = "";
+        tieler[legand(317)](legand(371)), tieler.on(legand(333), sugeiri => {
+          inese += sugeiri;
+        }), tieler.on(legand(351), () => {
+          const dalys = legand;
+          if (tieler[dalys(368)] !== 200) broddrick(new RestError(this[dalys(331)], _0x13fadb, inese)); else try {
+            celyna(JSON[dalys(340)](inese));
+          } catch (deazia) {
+            celyna(inese);
+          }
+        }), tieler.on(legand(370), neelesh => {
+          broddrick(new RestError(this.hostname, _0x13fadb, neelesh));
+        });
+      });
+      arnesto.on(crishon(370), brazen => {
+        const dabid = crishon;
+        broddrick(new RestError(this[dabid(331)], _0x13fadb, brazen));
+      }), arnesto[crishon(351)]();
+    });
+  }
+  ["put"](_0x442384, _0x454587) {
+    return this.post(_0x442384, _0x454587, "PUT");
+  }
+  [vedat(315)](_0x136bc7, _0x765283) {
+    const oluwatofunmi = vedat;
+    let taiyah;
+    if (typeof _0x765283 === oluwatofunmi(332)) taiyah = {"Access-Control-Request-Method": _0x765283}; else typeof _0x765283 === oluwatofunmi(363) ? taiyah = _0x765283 : taiyah = {};
+    return this[oluwatofunmi(374)](_0x136bc7, oluwatofunmi(318), taiyah);
+  }
+  get [vedat(369)]() {
+    const moa = vedat;
+    return this[moa(358)];
+  }
+  set [vedat(369)] (_0x3d670f) {
+    const andreea = vedat;
+    this[andreea(358)] = _0x3d670f;
+    if (this[andreea(360)] !== null) {
+      if (_0x3d670f > 0) clearInterval(this[andreea(360)]), this.delay_handle = setInterval(scheduler.bind(this), this[andreea(358)]); else {
+        this[andreea(360)] = null;
+        while (queue[andreea(357)]) scheduler();
+      }
+    }
+  }
+};
+function diavian(charmeka, hazelmarie) {
+  const mirabel = dahquan();
+  return diavian = function (mekka, annaclara) {
+    mekka = mekka - 314;
+    let itati = mirabel[mekka];
+    return itati;
+  }, diavian(charmeka, hazelmarie);
+}
+module[vedat(338)][vedat(374)] = function (reality, vasken) {
+  const neishaly = vedat;
+  let patrickjohn = new Rest(reality);
+  return patrickjohn[neishaly(374)](vasken);
+}, module[vedat(338)][vedat(350)] = function (teairra, neva, danek) {
+  const kearri = vedat;
+  let jashanna = new Rest(teairra);
+  return jashanna[kearri(350)](neva, danek);
+};
+const RestError = module[vedat(338)][vedat(367)] = class RestError extends Error {
+  constructor(_0x5985ab, _0x32a358, ..._0x2f4387) {
+    const brejon = vedat;
+    super(..._0x2f4387), this.hostname = _0x5985ab, this[brejon(365)] = _0x32a358, Error[brejon(329)] && Error.captureStackTrace(this, RestError), this.name = brejon(367);
+  }
+  [vedat(316)]() {
+    const kendricks = vedat;
+    return kendricks(319) + this.hostname + kendricks(364) + this[kendricks(365)] + kendricks(325) + this[kendricks(345)] + '"';
+  }
+};
