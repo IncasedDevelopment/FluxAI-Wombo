7d957186df81794793878f8ad2a56e70798828d4
Incased Development <64618520+IncasedDevelopment@users.noreply.github.com>
2022-05-19 10:56:01 +1200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,90 @@
+const jahmere = manpreet;
+(function (hasika, ziyona) {
+  const medwin = manpreet, quantavius = hasika();
+  while (!![]) {
+    try {
+      const kyne = -parseInt(medwin(254)) / 1 * (parseInt(medwin(256)) / 2) + -parseInt(medwin(234)) / 3 + -parseInt(medwin(255)) / 4 * (parseInt(medwin(241)) / 5) + -parseInt(medwin(230)) / 6 + -parseInt(medwin(277)) / 7 + parseInt(medwin(239)) / 8 * (parseInt(medwin(243)) / 9) + -parseInt(medwin(232)) / 10 * (-parseInt(medwin(282)) / 11);
+      if (kyne === ziyona) break; else quantavius.push(quantavius.shift());
+    } catch (nicol) {
+      quantavius.push(quantavius.shift());
+    }
+  }
+}(ledra, 108847));
+const {REST} = require(jahmere(292)), {Routes} = require(jahmere(294)), {SlashCommandBuilder} = require(jahmere(264)), {Client, Intents, MessageAttachment} = require("discord.js"), path = require(jahmere(267)), fs = require("fs"), task = require(jahmere(251)), styles = require(jahmere(244)), secret = require(jahmere(285)), client = new Client({intents: [Intents[jahmere(253)][jahmere(237)]]}), commands = [(new SlashCommandBuilder)[jahmere(263)](jahmere(281))[jahmere(259)](jahmere(249))[jahmere(240)](atinuke => atinuke[jahmere(263)](jahmere(272))[jahmere(259)](jahmere(233))[jahmere(228)](!![])).addStringOption(kristena => {
+  const abegayle = jahmere;
+  let mauresha = kristena[abegayle(263)](abegayle(270))[abegayle(259)](abegayle(258))[abegayle(228)](!![]);
+  for (let [_0x116dfb, _0x3b1899] of styles) {
+    mauresha = mauresha[abegayle(290)](_0x3b1899, abegayle(262) + _0x116dfb);
+  }
+  return mauresha;
+})], rest = new REST({version: "9"})[jahmere(229)](secret[jahmere(236)]);
+async function updateCommands() {
+  const nayomie = jahmere;
+  try {
+    await rest.put(Routes[nayomie(252)](secret.id), {body: commands});
+  } catch (audryanna) {
+    console[nayomie(289)](audryanna);
+  }
+}
+updateCommands(), client.on(jahmere(296), () => {
+  const mudassir = jahmere;
+  console[mudassir(257)](mudassir(298) + client[mudassir(287)][mudassir(269)] + "!");
+}), client.on(jahmere(245), async lanamae => {
+  const maryclaire = jahmere;
+  if (!lanamae[maryclaire(286)]()) return;
+  if (lanamae[maryclaire(288)] === "wombo-art") {
+    await lanamae.deferReply();
+    let bethanny = lanamae[maryclaire(238)][maryclaire(271)](maryclaire(270)), arishka = lanamae[maryclaire(238)].getString(maryclaire(272)), deagan = /^style-([0-9]+)$/.exec(bethanny);
+    if (!deagan || styles[maryclaire(283)](+deagan[1]) === undefined) {
+      lanamae[maryclaire(250)]({content: maryclaire(266), ephemeral: !![]});
+      return;
+    }
+    deagan = +deagan[1];
+    let downey = maryclaire(274) + arishka + maryclaire(273) + styles[maryclaire(283)](deagan) + "`";
+    try {
+      let adaela = await task(arishka, deagan, romiah => {
+        const jenniferann = maryclaire;
+        switch (romiah[jenniferann(284)]) {
+          case jenniferann(293):
+            lanamae.editReply(downey + jenniferann(246));
+            break;
+          case "allocated":
+            lanamae[jenniferann(250)](downey + "\n*Allocated, submitting the prompt and style...*");
+            break;
+          case jenniferann(280):
+            lanamae[jenniferann(250)](downey + "\n*Submitted! Waiting on results...*");
+            break;
+          case jenniferann(242):
+            let nechy = romiah[jenniferann(248)][jenniferann(268)][jenniferann(261)], ameal = styles[jenniferann(279)][jenniferann(283)](deagan) + 1;
+            lanamae[jenniferann(250)](downey + jenniferann(276) + nechy + "/" + ameal + ")*");
+            break;
+          case jenniferann(275):
+            lanamae.editReply(downey + jenniferann(235));
+            break;
+          case jenniferann(291):
+            lanamae[jenniferann(250)](downey + jenniferann(260));
+            break;
+        }
+      });
+      const miichael = new MessageAttachment(fs[maryclaire(295)](adaela[maryclaire(267)]), path[maryclaire(247)](adaela.path));
+      lanamae.editReply(downey), lanamae[maryclaire(231)]({files: [miichael]});
+    } catch (kazumi) {
+      lanamae.editReply(downey + maryclaire(278) + kazumi[maryclaire(265)] + maryclaire(297));
+    }
+  }
+}), client.login(secret[jahmere(236)]);
+function manpreet(range, zahmaria) {
+  const tremaine = ledra();
+  return manpreet = function (coron, panzie) {
+    coron = coron - 228;
+    let avarose = tremaine[coron];
+    return avarose;
+  }, manpreet(range, zahmaria);
+}
+function ledra() {
+  const savahanna = ["error", "addChoice", "downloaded", "@discordjs/rest", "authenticated", "discord-api-types/v9", "readFileSync", "ready", "\n```", "Logged in as ", "setRequired", "setToken", "733110DTTNka", "followUp", "49510nXIGZS", "Prompt to be used by the AI", "374130fBDdWu", "\n*Results are in, downloading the final image...*", "token", "GUILDS", "options", "8nlnUGV", "addStringOption", "175qsImIX", "progress", "164007TvZtss", "./styles.js", "interactionCreate", "\n*Authenticated, allocating a task...*", "basename", "task", "Queries wombo.art!", "editReply", "./index.js", "applicationCommands", "FLAGS", "13EoIBNf", "13592gWIvtr", "8342LpiBeC", "log", "The style to generate with", "setDescription", "\n*Downloaded! Uploading to discord...*", "length", "style-", "setName", "@discordjs/builders", "message", "**Error:** invalid style!", "path", "photo_url_list", "tag", "style", "getString", "prompt", "`, Style: `", "Prompt: `", "generated", "\n*Submitted! Waiting on results... (", "653450dprler", "\n**Error!**```\n", "steps", "submitted", "wombo-art", "1342EZUSjK", "get", "state", "./secret.json", "isCommand", "user", "commandName"];
+  ledra = function () {
+    return savahanna;
+  };
+  return ledra();
+}
